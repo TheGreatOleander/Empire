93b4c0ec55db0d4ba58e6da64cfb5a30bb517bb3
TheGreatOleander <thegreatoleander@gmail.com>
2025-06-18 03:42:41 -0400

    Delete repos/repo directory

--- repos/repo/CodeSync-Bridge/customize.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Customizing module (placeholder)"
-exit 0
--- repos/repo/CodeSync-Bridge/validate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Validating CodeSync setup..."
-if [ -f setup.sh ]; then
-  echo "setup.sh found, passing test"
-  exit 0
-else
-  echo "setup.sh missing, failing test"
-  exit 1
-fi
